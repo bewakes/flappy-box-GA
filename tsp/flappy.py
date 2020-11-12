ddc1d0abfc2ed174ec1216f954ef21db8269bf83
bewakes <bewakepandey@gmail.com>
2020-11-12 15:11:05 +0545

    Auto run based on chromosomes

--- tsp/flappy.py
+++ tsp/flappy.py
@@ -1,6 +1,7 @@
-import tkinter as tk
-
+import math
 import random
+
+import tkinter as tk
 
 FPS = 30
 FRAME_INTERVAL_MS = round(1000/FPS)
@@ -14,6 +15,21 @@ SPEED = 8
 XSTEP = OBSTACLES_WIDTH + OBSTACLES_H_SPACING
 FLAPPY_WIDTH = 28
 FLAPPY_HEIGHT = 25
+G = 0.6
+
+# For GA chromosomes
+
+MIN_V = -40
+MAX_V = 40
+
+MIN_DX = -OBSTACLES_WIDTH  # Minimum distance that can be between bird and nearest obstacle
+MAX_DX = OBSTACLES_H_SPACING  # Max distance that can be between bird and nearest obstacle
+
+CHROMOSOME_STEP = 10  # This is for the array size
+
+VSIZE = (MAX_V - MIN_V) // CHROMOSOME_STEP + 1
+DYSIZE = 2*HEIGHT // CHROMOSOME_STEP + 1  # [-height, -height + 10, ... , 0, 10, 20, ... height]
+DXSIZE = (MAX_DX - MIN_DX) // CHROMOSOME_STEP + 1
 
 
 def generate_obstacle(x: int):
@@ -32,19 +48,108 @@ def bounding_rect_collision(rect_a, rect_b):
            (ax2 <= bx2 and ax2 >= bx1 and ay2 <= by2 and ay2 >= by1)
 
 
-class Flappy(tk.Frame):
-    def __init__(self):
+class Flappy():
+    def __init__(self, x=20, y=HEIGHT/2, v=0, chromosome=[]):
+        self.dead = False
+        self.init_x, self.init_y, self.init_v = x, y, v
+        self.y = y
+        self.v = v
+        self.chromosome = chromosome
+        self.score = 0
+
+    def reset(self):
+        self.score = 0
+        self.dead = 0
+        self.x = self.init_x
+        self.y = self.init_y
+        self.v = self.init_v
+
+    def should_jump(self, obstacles, curr_x):
+        if not self.chromosome:
+            return False
+
+        # Get nearest obstacle
+        nearest_obstacle = obstacles[0]
+        for obstacle in obstacles:
+            if self.x < curr_x(obstacle[0][0]):
+                nearest_obstacle = obstacle
+                break
+
+        nearest_obstacle = obstacles[0]
+        nearest_obstacle_upper_rect = nearest_obstacle[0]
+
+        dx = self.x - nearest_obstacle_upper_rect[0]
+        upper_dy = self.y - nearest_obstacle_upper_rect[3]
+
+        i = math.ceil(dx - MIN_DX) // CHROMOSOME_STEP
+        j = math.ceil(upper_dy + HEIGHT) // CHROMOSOME_STEP
+        k = math.ceil(self.v - MIN_V) // CHROMOSOME_STEP
+        index = k * (DYSIZE * DXSIZE) + (j * DXSIZE + i)
+        if index > len(self.chromosome):
+            print('index greater')
+            return False
+        return self.chromosome[index] == 1
+
+    def jump(self):
+        self.v = -8
+
+    def did_collide(self, obstacles, curr_x):
+        return any([
+            bounding_rect_collision(
+                (self.x, self.y, self.x + FLAPPY_WIDTH, self.y + FLAPPY_HEIGHT),
+                (curr_x(x1), y1, curr_x(x2), y2)
+            )
+            for obs in obstacles for (x1, y1, x2, y2) in obs
+        ]) or self.y > HEIGHT or self.y < -FLAPPY_HEIGHT
+
+    def update(self, obstacles, curr_x):
+        if self.dead:
+            return
+        if self.did_collide(obstacles, curr_x):
+            self.dead = True
+
+        if self.should_jump(obstacles, curr_x):
+            self.jump()
+
+        self.v += G
+        self.y += self.v
+        self.score += 1
+
+
+class Game(tk.Frame):
+    def __init__(self, play=True, birds=[]):
         self.master = tk.Tk()
         super().__init__(self.master)
-        self.master.bind("<Key>", self.on_key)
-        self.reset()
+        self.generation = 0
+
+        self.play = play
+        if self.play:
+            self.master.bind("<Key>", self.on_key)
+
+        self.birds = birds or [Flappy()]
+
         self.setup()
+        self.reset()
         self.animate()
+
+    def rerun(self, birds):
+        self.birds = birds
+        self.generation += 1
+        self.reset()
+
+    def reset(self):
+        self.obstacles = [
+            generate_obstacle(x)
+            for x in range(300, 1000, OBSTACLES_WIDTH + OBSTACLES_H_SPACING)
+        ]
+        [bird.reset() for bird in self.birds]
+        self.stop = False
+        self.time = 0
 
     def setup(self):
         self.canvas = tk.Canvas(self.master, width=WIDTH, height=HEIGHT, bg='white')
         self.canvas.pack(pady=10)
-        self.button = tk.Button(self.master, text="Restart", command=self.reset)
+        self.button = tk.Button(self.master, text="Restart", command=self.restart)
         self.button.pack(pady=10)
         self.score_label = tk.Label(self.master, text="Score")
         self.score_label.pack(pady=10)
@@ -52,35 +157,18 @@ class Flappy(tk.Frame):
         self.score_container = tk.Label(self.master, textvariable=self.score)
         self.score_container.pack()
 
-    def did_collide(self):
-        return any([
-            bounding_rect_collision(
-                (self.x, self.y, self.x + FLAPPY_WIDTH, self.y + FLAPPY_HEIGHT),
-                (self.curr_x(x1), y1, self.curr_x(x2), y2)
-            )
-            for obs in self.obstacles for (x1, y1, x2, y2) in obs
-        ]) or self.y > HEIGHT or self.y < -FLAPPY_HEIGHT
+    def restart(self):
+        self.reset()
+        self.animate()
 
     def on_key(self, event):
         if event.keysym == 'space':
-            self.v = -8
+            self.birds[0].jump()
 
-    def reset(self):
-        self.time = 0
-
-        self.obstacles = [
-            generate_obstacle(x)
-            for x in range(300, 1000, OBSTACLES_WIDTH + OBSTACLES_H_SPACING)
-        ]
-        self.a = 0.6
-        self.v = 0
-        self.x = 20
-        self.y = HEIGHT / 2
-
-    def draw_flappy(self):
+    def draw_flappy(self, bird):
         self.canvas.create_rectangle(
-            self.x, self.y,
-            self.x + FLAPPY_WIDTH, self.y + FLAPPY_HEIGHT,
+            bird.x, bird.y,
+            bird.x + FLAPPY_WIDTH, bird.y + FLAPPY_HEIGHT,
             fill='green'
         )
 
@@ -98,20 +186,19 @@ class Flappy(tk.Frame):
     def draw(self):
         self.canvas.delete('all')
         self.draw_obstacles()
-        self.draw_flappy()
-        # Set score
-        self.score.set(str(self.time))
+
+        if self.play:
+            self.draw_flappy(self.birds[0])
+            self.score.set(str(self.time))
+        else:
+            [self.draw_flappy(bird) for bird in self.birds if not bird.dead]
 
     def curr_x(self, x):
         return x - self.time * SPEED
 
     def update(self):
-        if self.did_collide():
-            return
-
-        self.v += self.a
-        self.y += self.v
         self.time += 1
+        [bird.update(self.obstacles, self.curr_x) for bird in birds]
 
         # Filter obstacles and create new if necessary
         self.obstacles = [
@@ -135,11 +222,16 @@ class Flappy(tk.Frame):
             ]
 
     def animate(self):
+        if self.stop or (self.play and self.birds[0].dead):
+            return
         self.draw()
         self.update()
         self.master.after(FRAME_INTERVAL_MS, self.animate)
 
 
 if __name__ == '__main__':
-    flappy = Flappy()
-    flappy.mainloop()
+    def chrom_rand():
+        return [random.choice([0, 1]) for _ in range(VSIZE*DYSIZE*DXSIZE)]
+    birds = [Flappy(v=-random.random(), y=random.randrange(HEIGHT//4, 3*HEIGHT//4), chromosome=chrom_rand()) for _ in range(10)]
+    game = Game(play=False, birds=birds)
+    game.mainloop()
